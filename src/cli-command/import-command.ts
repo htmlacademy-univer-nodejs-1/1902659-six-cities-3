94772ae74ba9b32b5a356ee44d9f4716bfc76eb1
c00b3r <sanuyOKmaklaren0>
2024-12-21 19:26:20 +0500

    feat: implement import command

--- src/cli-command/import-command.ts
+++ src/cli-command/import-command.ts
@@ -1,3 +1,6 @@
+import chalk from "chalk";
+import TSVFileReader from "../file-reader/file-reader.js";
+import { getErrorMessage } from "../utils/common.js";
 import { CLiCommand } from "./cli-command.interface.js";
 
 export default class ImportCommand implements CLiCommand {
@@ -5,5 +8,14 @@ export default class ImportCommand implements CLiCommand {
 
   public execute(filename: string): void {
     const fileReader = new TSVFileReader(filename.trim());
+
+    try {
+      fileReader.read();
+      console.log(fileReader.parseData());
+    } catch (error) {
+      console.log(
+        `${chalk.redBright(`Нельзя прочитать файл: ${getErrorMessage(error)}`)}`
+      );
+    }
   }
 }
