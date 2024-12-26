687601e6018e7159a780d0c63403f2184e88215d
c00b3r <sanuyOKmaklaren0>
2024-12-27 00:07:09 +0500

    feat: implement rest config

--- src/config/rest.config.ts
+++ src/config/rest.config.ts
@@ -1,11 +1,28 @@
 import { inject, injectable } from "inversify";
 import { Config } from "./config.interface.js";
-import { RestSchema } from "./rest.schema.js";
+import { configRestSchema, RestSchema } from "./rest.schema.js";
 import { Component } from "../types/component.enum.js";
+import { Logger } from "../logger/logger.interface.js";
+import { config } from "dotenv";
 
 @injectable()
 export default class RestConfig implements Config<RestSchema> {
   private readonly config: RestSchema;
 
-  constructor(@inject(Component.Logger) private readonly logger: Logger)
+  constructor(@inject(Component.Logger) private readonly logger: Logger) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error("Невозможно прочить env-файл");
+    }
+
+    configRestSchema.load({});
+    configRestSchema.validate({ allowed: "strict", output: this.logger.info });
+    this.config = configRestSchema.getProperties();
+    this.logger.info(".env файд найден и прочитан");
+  }
+
+  public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
+    return this.config[key];
+  }
 }
--- src/config/rest.schema.ts
+++ src/config/rest.schema.ts
@@ -1,5 +1,28 @@
+import convict from "convict";
+
 export type RestSchema = {
   PORT: number;
   SALT: string;
   DB_HOST: string;
 };
+
+export const configRestSchema = convict<RestSchema>({
+  PORT: {
+    doc: "Port to bind",
+    format: "port",
+    env: "PORT",
+    default: 4000,
+  },
+  SALT: {
+    doc: "Salt for bcrypt",
+    format: String,
+    default: null,
+    env: "SALT",
+  },
+  DB_HOST: {
+    doc: "Database host",
+    format: "ipaddress",
+    default: "127.0.0.1",
+    env: "DB_HOST",
+  },
+});
