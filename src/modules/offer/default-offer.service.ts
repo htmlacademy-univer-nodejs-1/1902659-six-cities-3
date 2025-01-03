0e90f2e53886133fe599a7ec8911eea9ecf173ea
c00b3r <sanuyOKmaklaren0>
2025-01-03 17:12:51 +0500

    feat: update offer service

--- src/modules/offer/default-offer.service.ts
+++ src/modules/offer/default-offer.service.ts
@@ -5,6 +5,9 @@ import { Logger } from "../../logger/logger.interface.js";
 import { DocumentType, types } from "@typegoose/typegoose";
 import { OfferEntity } from "./offer.entity.js";
 import CreateOfferDto from "./dto/create-offer.dto.js";
+import UpdateOfferDto from "./dto/update-offer.dto.js";
+import { DEFAULT_OFFER_COUNT } from "./offer.constant.js";
+import { Sort } from "../../types/sort-type.emum.js";
 
 @injectable()
 export default class DefaultOfferService implements OfferService {
@@ -24,5 +27,82 @@ export default class DefaultOfferService implements OfferService {
     offerId: string
   ): Promise<DocumentType<OfferEntity> | null> {
     return this.offerModel.findById(offerId).exec();
+  }
+
+  public async find(count?: number): Promise<DocumentType<OfferEntity>[]> {
+    const limitCount = count ?? DEFAULT_OFFER_COUNT;
+    return this.offerModel
+      .find()
+      .sort({ createdAt: Sort.Down })
+      .limit(limitCount)
+      .populate("offerId")
+      .exec();
+  }
+
+  public async deleteById(
+    offerId: string
+  ): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel.findByIdAndDelete(offerId).exec();
+  }
+
+  public async updateById(
+    offerId: string,
+    dto: UpdateOfferDto
+  ): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, dto, { new: true })
+      .populate("offerId")
+      .exec();
+  }
+
+  public async getDetailsInfo(
+    offerId: string
+  ): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel.findById(offerId).exec();
+  }
+
+  public async incCommentCount(
+    offerId: string
+  ): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, {
+        $inc: {
+          commentCount: 1,
+        },
+      })
+      .exec();
+  }
+
+  public async getPremium(): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find({ flagIsPremium: true })
+      .populate("offerId")
+      .exec();
+  }
+
+  public async getFavorite(): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find({ flagIsFavourites: true })
+      .populate("offerId")
+      .exec();
+  }
+
+  public async calculationRating(
+    rating: number,
+    newRating: number,
+    countRating: number,
+    offerId: string
+  ): Promise<void> {
+    await this.offerModel
+      .findByIdAndUpdate(
+        offerId,
+        { rating: (newRating + rating) / countRating },
+        { new: true }
+      )
+      .exec();
+  }
+
+  public async exists(documentId: string): Promise<boolean> {
+    return (await this.offerModel.exists({ _id: documentId })) !== null;
   }
 }
