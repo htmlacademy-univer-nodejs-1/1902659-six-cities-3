cf94aa242c3287b6a1cf1de54f71a9b414b7a2cb
c00b3r <sanuyOKmaklaren0>
2025-01-04 18:20:32 +0500

    feat: add user controller and implement init application

--- src/modules/offer/offer.container.ts
+++ src/modules/offer/offer.container.ts
@@ -4,6 +4,8 @@ import { Component } from "../../types/component.enum.js";
 import DefaultOfferService from "./default-offer.service.js";
 import { types } from "@typegoose/typegoose";
 import { OfferEntity, OfferModel } from "./offer.entity.js";
+import { BaseController } from "../../controller/base-controller.js";
+import OfferController from "./controller/offer.controller.js";
 
 export function createOfferContainer() {
   const offerConatiner = new Container();
@@ -14,6 +16,10 @@ export function createOfferContainer() {
   offerConatiner
     .bind<types.ModelType<OfferEntity>>(Component.OfferModel)
     .toConstantValue(OfferModel);
+  offerConatiner
+    .bind<BaseController>(Component.OfferController)
+    .to(OfferController)
+    .inSingletonScope();
 
   return offerConatiner;
 }
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -5,15 +5,28 @@ import { Config } from "../config/config.interface.js";
 import { RestSchema } from "../config/rest.schema.js";
 import { DatabaseClient } from "../database-client/database-client.interface.js";
 import { getMongoURI } from "../utils/getMongoURL.js";
+import { ExceptionFilter } from "../exception-filters/exception-filters.interface.js";
+import { BaseController } from "../controller/base-controller.js";
+import express, { Express } from "express";
 
 @injectable()
 export default class Application {
+  private server: Express;
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient)
-    private readonly databaseClient: DatabaseClient
-  ) {}
+    private readonly databaseClient: DatabaseClient,
+    @inject(Component.AppExceptionFilter)
+    private readonly exceptionFilter: ExceptionFilter,
+    @inject(Component.UserController)
+    private readonly userController: BaseController,
+    @inject(Component.OfferController)
+    private readonly offerController: BaseController
+  ) {
+    this.server = express();
+  }
 
   private async _initDb() {
     const mongoUrl = getMongoURI(
@@ -27,11 +40,49 @@ export default class Application {
     return this.databaseClient.connect(mongoUrl);
   }
 
+  private async _initServer() {
+    this.logger.info("Try to init server");
+
+    const port = this.config.get("PORT");
+    this.server.listen(port);
+
+    this.logger.info(
+      `Server started on http://localhost:${this.config.get("PORT")}`
+    );
+  }
+
+  private async _initControllers() {
+    this.logger.info("Controller init");
+
+    this.server.use("/users", this.userController.router);
+    this.server.use("/offers", this.offerController.router);
+
+    this.logger.info("Controller completed");
+  }
+
+  private async _initMiddleware() {
+    this.logger.info("Init middleware");
+
+    this.server.use(express.json());
+
+    this.logger.info("Middleware init completed");
+  }
+
+  private async _initExceptionFilters() {
+    this.logger.info("Init exception filters");
+
+    this.server.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+
+    this.logger.info("Exception filters completed");
+  }
+
   public async init() {
-    this.logger.info("Приложение запущено");
-    this.logger.info(`REST-сервер запущен на порту ${this.config.get("PORT")}`);
-    this.logger.info("Запуск базы данных...");
+    this.logger.info("Init application");
+
     await this._initDb();
-    this.logger.info("База данных запущена");
+    await this._initMiddleware();
+    await this._initExceptionFilters();
+    await this._initServer();
+    await this._initControllers();
   }
 }
